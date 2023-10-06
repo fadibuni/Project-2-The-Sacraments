bb13670429c538e210ccb767e8007fa728bca864
Fadi Buni <fadibuni1@gmail.com>
2023-10-06 09:23:49 -0700

    created the new project

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
-# Web Development Project 2 - *THe Sacramnets of the Catholic Church*
+# Web Development Project 3 - *The Sacramnets Continued*
 
 Submitted by: **Fadi Buni**
 
-This web app: **This app uses flashcards to ask questions about the matter of the seven sacraments of the Catholic Church**
+This web app: **Adds quiz and shuffle alonside other features to my previous project**
 
-Time spent: **5** hours spent in total
+Time spent: **4** hours spent in total
 
 ## Required Features
 
 The following **required** functionality is completed:
 
-- [x] **The title of the card set and some information about it, such as a short description and the total number of cards are displayed**
-- [x] **A single card at a time is displayed, only showing one of the components of the information pair**
-- [x] **A list of card pairs is created**
-- [x] **Clicking on the card shows the corresponding component of the information pair**
-- [x] **Clicking the next button displays a random new card**
+- [X] **The user can enter their guess in a box before seeing the flipside of the card**
+- [X] **Clicking on a submit button shows visual feedback about whether the answer was correct or incorrect**
+- [X] **A back button is displayed on the card and can be used to return to the previous card in a set sequence**
+- [X] **A next button is displayed on the card and can be used to navigate to the next card in a set sequence**
 
 The following **optional** features are implemented:
 
-- [ ] Cards contains images in addition to or in place of text
-- [x] Cards have different visual styles such as color based on their category
-  - [x] *visual style implemented*
+- [X] A shuffle button is used to randomize the order of the cards
+- [X] A user's answer may be counted as correct even when it is slightly different from the target answer
+- [X] A counter displays the user's current and longest streak of correct responses
+- [X] A user can mark a card that they have mastered and have it removed from the pool of answers as well as added to a list of mastered cards
 
 The following **additional** features are implemented:
 
@@ -28,13 +28,13 @@ The following **additional** features are implemented:
 
 ## Video Walkthrough
 
-Here's a walkthrough of implemented required features:
+Here's a walkthrough of implemented user stories:
 
-https://imgur.com/a/KzLCJyO
-
+<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
 
 <!-- Replace this with whatever GIF tool you used! -->
-GIF created with screentogif 
+GIF created with screenToGIF
+<blockquote class="imgur-embed-pub" lang="en" data-id="a/ikxz5Xu" data-context="false" ><a href="//imgur.com/a/ikxz5Xu"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
 <!-- Recommended tools:
 [Kap](https://getkap.co/) for macOS
 [ScreenToGif](https://www.screentogif.com/) for Windows
--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,135 @@ import FlashCard from "./components/FlashCard";
 import cardPairs from "./assets/cardPairs";
 import "./App.css";
 
+const cleanString = (str) => str.trim().toLowerCase();
+
 const App = () => {
-  const [currentIndex, setCurrentIndex] = useState(-1);
-  const [isFlipped, setIsFlipped] = useState(false);
+  const congratsCard = {
+    question: "Congratulations!",
+    answer: "You have mastered all the cards.",
+  };
 
   const firstCard = {
     question: "Start!",
     answer: "Press the next arrow to start the flashcards :)",
-  }
-  const nextCard = () => {
-    let nextIndex = Math.floor(Math.random() * cardPairs.length);
-    while (nextIndex === currentIndex && cardPairs.length > 1) {
-      nextIndex = Math.floor(Math.random() * cardPairs.length);
+  };
+
+  const shuffleArray = (array) => {
+    let shuffled = array.slice();
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
-    setCurrentIndex(nextIndex);
-    setIsFlipped(false);
+    return [firstCard, ...shuffled];
+  };
+
+  const [state, setState] = useState({
+    currentIndex: 0,
+    isFlipped: false,
+    userGuess: "",
+    feedback: null,
+    currentStreak: 0,
+    longestStreak: 0,
+    cards: shuffleArray(cardPairs)
+  });
+
+  const [masteredCards, setMasteredCards] = useState([]);
+
+  const checkAnswer = () => {
+    let newState = { ...state, isFlipped: true };
+
+    if (cleanString(state.userGuess) === cleanString(state.cards[state.currentIndex].answer)) {
+      newState.currentStreak = state.currentStreak + 1;
+      newState.longestStreak = Math.max(state.longestStreak, newState.currentStreak);
+      newState.feedback = true;
+    } else {
+      newState.currentStreak = 0;
+      newState.feedback = false;
+    }
+
+    setState(newState);
+  };
+
+  const nextCard = () => {
+    setState({
+      ...state,
+      currentIndex: (state.currentIndex + 1) % state.cards.length,
+      isFlipped: false,
+      feedback: null,
+      userGuess: ""
+    });
+  };
+
+  const prevCard = () => {
+    setState({
+      ...state,
+      currentIndex: state.currentIndex === 0 ? state.cards.length - 1 : state.currentIndex - 1,
+      isFlipped: false,
+      feedback: null,
+      userGuess: ""
+    });
+  };
+
+  const shuffleCards = () => {
+    setState({
+      ...state,
+      cards: shuffleArray(cardPairs),
+      currentIndex: 0
+    });
+  };
+
+  const markAsMastered = () => {
+    if (state.currentIndex === 0) {
+      nextCard();
+      return;
+    }
+
+    const masteredCard = state.cards[state.currentIndex];
+    setMasteredCards([...masteredCards, masteredCard]);
+
+    const newCards = state.cards.filter((card, index) => index !== state.currentIndex);
+    
+    if (newCards.length === 1) {
+      newCards.push(congratsCard);
+    }
+
+    setState({
+      ...state,
+      cards: newCards,
+      currentIndex: 0
+    });
   };
 
   return (
     <div className="App">
-      <h2>The Seven Sacraments Quiz!</h2>
-      <h4>
-        Do you know what is the matter of each of the sacraments of the Catholic
-        church? Test your knowledge here!
-      </h4>
-      <h5>Number of cards: {cardPairs.length}</h5>
+      <div className="header">
+        <h1>The Ultimate Sacrament Scholar!</h1>
+        <h2>How well do you know the sacraments of the Catholic Church?</h2>
+        <h4>Test your sacrament knowledge here!</h4>
+        <h5>Number of cards: {state.cards.length}</h5>
+        <h5>Current Streak: {state.currentStreak}, Longest Streak: {state.longestStreak}</h5>
+      </div>
       <br />
-      {currentIndex === -1 ? (
-        <FlashCard
-          card={firstCard}
-          isFlipped={isFlipped}
-          setIsFlipped={setIsFlipped}
-        />
-      ) : (
-        <FlashCard
-          card={cardPairs[currentIndex]}
-          isFlipped={isFlipped}
-          setIsFlipped={setIsFlipped}
-        />
-      )}
+      <FlashCard
+        card={state.cards[state.currentIndex]}
+        isFlipped={state.isFlipped}
+        setIsFlipped={isFlipped => setState({ ...state, isFlipped })}
+      />
+      <input
+        type="text"
+        value={state.userGuess}
+        onChange={(e) => setState({ ...state, userGuess: e.target.value })}
+        placeholder="Enter your guess"
+      />
+      <button onClick={checkAnswer} type="button">Submit</button>
+      {state.feedback !== null && <div>{state.feedback ? "Correct!" : "Incorrect!"}</div>}
       <br />
-      <button onClick={nextCard} type="button">
-        ⭢
-      </button>
+      <button onClick={prevCard} type="button">⭠</button>
+      <button onClick={nextCard} type="button">⭢</button>
+      <button onClick={shuffleCards} type="button">Shuffle</button>
+      <button onClick={markAsMastered} type="button" >Mark as Mastered</button>
+      <div style={{ color: 'white' }}>Mastered Cards: {masteredCards.length}</div>
+      {masteredCards.map((card, index) => <div style={{ color: 'white' }} key={index}>{card.question}</div>)}
     </div>
   );
 };
